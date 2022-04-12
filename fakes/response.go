85c482d443c6cecaa1221af0a6c6b97db855ba6d
Travis Harmon <harmtrav@gmail.com>
2022-04-11 22:47:40 -0500

    Add docs to the fakes

--- fakes/response.go
+++ fakes/response.go
@@ -7,11 +7,14 @@ import (
 	"github.com/twharmon/golamb"
 )
 
+// Response implements the golamb.Responder interface.
 type Response struct {
 	response *events.APIGatewayV2HTTPResponse
 	err      error
 }
 
+// NewResponse creates a value that implements the golamb.Responder
+// interface.
 func NewResponse() *Response {
 	return &Response{
 		response: &events.APIGatewayV2HTTPResponse{
@@ -20,15 +23,18 @@ func NewResponse() *Response {
 	}
 }
 
+// Response implements the golamb.Responder interface.
 func (r *Response) Respond() (*events.APIGatewayV2HTTPResponse, error) {
 	return r.response, r.err
 }
 
+// SetCookie implements the golamb.Responder interface.
 func (r *Response) SetCookie(cookie *http.Cookie) golamb.Responder {
 	r.response.Cookies = append(r.response.Cookies, cookie.String())
 	return r
 }
 
+// SetHeader implements the golamb.Responder interface.
 func (r *Response) SetHeader(key string, val string) golamb.Responder {
 	r.response.Headers[key] = val
 	return r
